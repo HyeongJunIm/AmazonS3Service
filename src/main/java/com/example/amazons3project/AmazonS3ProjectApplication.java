4f8a9d77bf02af81e6ade0a9e52574a8508ca993
HyeongJunIm <chaesng6064@gmail.com>
2024-04-03 11:45:39 +0900

    commit

---
+++ src/main/java/com/example/amazons3project/AmazonS3ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.amazons3project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonS3ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmazonS3ProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/amazons3project/AmazonS3ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amazons3project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonS3ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
